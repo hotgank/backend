64974f8090d43de9c7085528bb3c567f7d873a01
qwq995 <3087675598@qq.com>
2024-11-17 18:48:35 +0800

    修改文章发布与获取逻辑

--- src/main/java/org/example/backend/controller/others/HealthArticleController.java
+++ src/main/java/org/example/backend/controller/others/HealthArticleController.java
@@ -1,7 +1,11 @@
 package org.example.backend.controller.others;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Date;
+import org.example.backend.entity.doctor.Doctor;
 import org.example.backend.entity.others.HealthArticle;
 import org.example.backend.service.others.HealthArticleService;
+import org.example.backend.service.doctor.DoctorService;
 import org.example.backend.util.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +22,48 @@ public class HealthArticleController {
   private HealthArticleService healthArticleService;
   @Autowired
   private JsonParser jsonParser;
+  @Autowired
+  private DoctorService doctorService;
   @PostMapping("/add")
-  public ResponseEntity<String> addHealthArticle(@RequestBody HealthArticle healthArticle) {
-    boolean result=healthArticleService.createHealthArticle(healthArticle);
+  public ResponseEntity<String> addHealthArticle(@RequestBody String healthArticle,
+      HttpServletRequest request) {
+    String doctorId = (String) request.getAttribute("userId");
+    HealthArticle healthArticle1=new HealthArticle();
+    healthArticle1.setDoctorId(doctorId);
+    healthArticle1.setTitle(jsonParser.parseJsonString(healthArticle,"title"));
+    healthArticle1.setContent(jsonParser.parseJsonString(healthArticle,"content"));
+    healthArticle1.setType(jsonParser.parseJsonString(healthArticle,"type"));
+    healthArticle1.setStatus("未审核");
+    healthArticle1.setPublishDate(new Date());
+    boolean result=healthArticleService.createHealthArticle(healthArticle1);
     if(result)
       return ResponseEntity.ok("Successfully commit health article");
     else return ResponseEntity.status(500).body("Failed to commit health article");
   }
   @PostMapping("/getAll")
   public ResponseEntity<String> getAllHealthArticle() {
-
+  String jsonString = jsonParser.toJsonFromEntityList(healthArticleService.getAll());
+  jsonString = jsonParser.removeKeyFromJson(jsonString, "doctorId");
   return ResponseEntity.ok(jsonParser.toJsonFromEntityList(healthArticleService.getAll())); }
+
+  @PostMapping("/getDoctorByArticleId")
+  public ResponseEntity<String> getDoctorByArticleId(@RequestBody String healthArticle) {
+    int articleId=jsonParser.parseJsonInt(healthArticle,"articleId");
+    if(healthArticleService.getById(articleId)==null)
+      return ResponseEntity.status(404).body("Not Found");
+    String doctorId=healthArticleService.getById(articleId).getDoctorId();
+    Doctor doctor=doctorService.selectById(doctorId);
+    String jsonString=jsonParser.toJsonFromEntity(doctor);
+    jsonString = jsonParser.removeKeyFromJson(jsonString, "password");
+    jsonString = jsonParser.removeKeyFromJson(jsonString, "doctorId");
+    jsonString = jsonParser.removeKeyFromJson(jsonString, "registrationDate");
+    jsonString = jsonParser.removeKeyFromJson(jsonString, "lastLogin");
+    jsonString = jsonParser.removeKeyFromJson(jsonString, "status");
+    jsonString = jsonParser.removeKeyFromJson(jsonString, "avatarUrl");
+    jsonString = jsonParser.removeKeyFromJson(jsonString, "phone");
+    jsonString = jsonParser.removeKeyFromJson(jsonString, "email");
+    jsonString = jsonString.substring(0,jsonString.length()-1);
+    return ResponseEntity.ok(jsonString+",\"avatar\":\"" + doctorService.getAvatarBase64(doctorId)+"\"}");
+  }
 
 }
